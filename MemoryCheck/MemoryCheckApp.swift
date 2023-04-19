756008c47bbb363ca5252bf6537895023d63eb5f
ShaniDan <smirabzalova88@gmail.com>
2023-04-19 12:08:15 -0700

    Initial Commit

---
+++ MemoryCheck/MemoryCheckApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryCheckApp.swift
+//  MemoryCheck
+//
+//  Created by Shakhnoza Mirabzalova on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCheckApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
